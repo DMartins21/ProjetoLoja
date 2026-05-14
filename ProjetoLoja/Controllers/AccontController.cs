e49ba8948450948e44d8d8a7af165598d45a962b
DMartins21 <davi_martins2009@outlook.com>
2026-05-14 17:57:27 -0300

    adicionando UserController

--- ProjetoLoja/Controllers/AccontController.cs
+++ ProjetoLoja/Controllers/AccontController.cs
@@ -1,6 +1,50 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjetoLoja.ViewModel;
+
 namespace ProjetoLoja.Controllers;
 
-public class AccontController
+public class AccontController : Controller
 {
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly SignInManager<IdentityUser> _signInManager;
     
+    public AccontController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+
+    public IActionResult Login(string returnUrl)
+    {
+        return View(new LoginViewModel()
+        {
+            ReturnUrl = returnUrl
+        });
+    }
+
+    public async Task<IActionResult> Login(LoginViewModel loginVm)
+    {
+        if (ModelState.IsValid)
+        {
+            return View(loginVm);
+        }
+
+        var user = await _userManager.FindByEmailAsync(loginVm.UserName);
+
+        if (user != null)
+        {
+            var result = await _signInManager.PasswordSignInAsync(user, loginVm.Password, false, false);
+            if (result.Succeeded)
+            {
+                if (string.IsNullOrEmpty(loginVm.ReturnUrl))
+                {
+                    return Redirect(loginVm.ReturnUrl);
+                }
+                return  Redirect(loginVm.ReturnUrl);
+            }
+        }
+        ModelState.AddModelError("", "Falha ao Realizar Login");
+        return View(loginVm);
+    }
 }
--- ProjetoLoja/ViewModel/LoginViewModel.cs
+++ ProjetoLoja/ViewModel/LoginViewModel.cs
@@ -1,6 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjetoLoja.ViewModel;
 
 public class LoginViewModel
 {
-    
+    [Required(ErrorMessage = "Informe Seu Usuário")]
+    [Display(Name = "Usuário")]
+    public string UserName { get; set; }
+    [Required(ErrorMessage = "Informe a Senha")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Senha")]
+    public string Password { get; set; }
+    public string ReturnUrl { get; set; }
 }
